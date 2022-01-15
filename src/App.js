75ae490c3b8f54775135fece82701ca9dbb11dd8
FaithChinonso <faithuchinonso@gmail.com>
2022-01-15 22:01:18 +0100

    third commit

--- src/App.js
+++ src/App.js
@@ -1,45 +1,55 @@
+import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expense";
+import NewExpense from "./components/NewExpense/NewExpense";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "el",
+    title: "Car Insurance",
+    amount: 14.23,
+    date: new Date(2023, 12, 29),
+  },
+  {
+    id: "em",
+    title: "Fuel",
+    amount: 194.09,
+    date: new Date(2022, 12, 28),
+  },
+  {
+    id: "en",
+    title: "Food",
+    amount: 24.6,
+    date: new Date(2022, 12, 27),
+  },
+  {
+    id: "eo",
+    title: "Clothes",
+    amount: 94.7,
+    date: new Date(2022, 12, 25),
+  },
+  {
+    id: "ep",
+    title: "TV",
+    amount: 994.7,
+    date: new Date(2022, 12, 22),
+  },
+];
 
 const App = () => {
-  const expenses = [
-    {
-      id: "el",
-      title: "Car Insurance",
-      amount: 14.23,
-      date: new Date(2022, 12, 29),
-    },
-    {
-      id: "em",
-      title: "Fuel",
-      amount: 194.09,
-      date: new Date(2022, 12, 28),
-    },
-    {
-      id: "en",
-      title: "Food",
-      amount: 24.6,
-      date: new Date(2022, 12, 27),
-    },
-    {
-      id: "eo",
-      title: "Clothes",
-      amount: 94.7,
-      date: new Date(2022, 12, 25),
-    },
-    {
-      id: "ep",
-      title: "TV",
-      amount: 994.7,
-      date: new Date(2022, 12, 22),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpenseData={addExpenseHandler} />
 
       <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
